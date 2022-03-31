3850389b5c664e567531094311622a1999ca7599
soon <soon_myeong@naver.com>
2022-03-31 23:08:00 +0900

    애나그램문제 풀어!!

--- src/test/java/algorithm/day06/_438/Solution.java
+++ src/test/java/algorithm/day06/_438/Solution.java
@@ -3,6 +3,7 @@ package algorithm.day06._438;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -36,11 +37,45 @@ import java.util.List;
  *  슬라이딩윈도우 알고리즘을 활용하는 대표적인 문제인 애나그램 문제
  *  으... 아직은 내게 어렵구나
  *
+ *  https://leetcode.com/problems/find-all-anagrams-in-a-string/
+ *
  */
 public class Solution {
 
     public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        int[] map = new int[26];
 
+        for(int i=0; i<p.length(); i++) {
+            map[p.charAt(i)-'a']++;
+        }
+
+        int start = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            map[s.charAt(i)-'a']--;
+
+            //슬라이딩 윈도우를 잘 떠올려 보자.
+            if(i-start >= p.length()) {
+                map[s.charAt(start) - 'a']++;
+                start++;
+            }
+
+            if(allZeroMap(map)) {
+                result.add(start);
+            }
+
+        }
+        return result;
+    }
+
+    private boolean allZeroMap(int[] map) {
+        for(int i=0; i<map.length; i++) {
+            if(map[i] != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -49,7 +84,15 @@ public class Solution {
         String s = "abab";
         String p = "ab";
 
-        findAnagrams(s,p);
+        findAnagrams(s,p).forEach(System.out::println);
+    }
+
+    @Test
+    void example2() {
+        String s = "cbaebabacd";
+        String p = "abc";
+
+        findAnagrams(s,p).forEach(System.out::println);
     }
 
 }
